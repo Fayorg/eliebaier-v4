92c7bca78e50037eafabcade16b50f0403b5fab8
Fayorg <ebr@aketechnology.com>
2025-08-29 22:43:52 +0200

    fix: link not working on desktop icon

--- components/navigation/mobile-navigation-logo.tsx
+++ components/navigation/mobile-navigation-logo.tsx
@@ -1,10 +1,13 @@
 import { Command, Menu } from 'lucide-react';
+import Link from 'next/link';
 
 export function MobileNavigationLogo({ isOpen = false, setOpen = (open: boolean) => {} }: { isOpen?: boolean; setOpen?: (open: boolean) => void }) {
 	return (
 		<div>
 			<div className="hidden md:block">
-				<Command className="w-5 h-5 text-primary" />
+				<Link className="flex items-center" href={'/'}>
+					<Command className="w-5 h-5 text-primary" />
+				</Link>
 			</div>
 			<div className="block md:hidden p-2 cursor-pointer" onClick={() => setOpen(!isOpen)}>
 				<Menu className="w-7 h-7 text-primary" />
